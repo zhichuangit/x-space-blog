e327079cb4d50da8c95c8966243856c01fde2c66
zhangxiantian <441741073@qq.com>
2026-08-20 18:42:44 +0800

    chore: remove temp script

--- scripts/cleanup.mjs
+++ scripts/cleanup.mjs
@@ -1,9 +1,8 @@
 import { execSync } from 'node:child_process';
 
 const cwd = 'd:/dev/x-space/x-space-blog';
-const msg = 'style: redesign blog list item layout';
 
 execSync('git add -A', { cwd, stdio: 'inherit' });
-execSync(`git commit -m "${msg}"`, { cwd, stdio: 'inherit' });
+execSync('git commit -m "chore: remove temp script"', { cwd, stdio: 'inherit' });
 execSync('git push origin main', { cwd, stdio: 'inherit' });
-console.log('OK: committed and pushed');
+console.log('OK: cleanup committed and pushed');
